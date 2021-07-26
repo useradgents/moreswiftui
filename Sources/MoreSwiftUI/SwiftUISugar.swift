61bf0c01ea7d4521517fd8cb6923d54f6b2fcdb6
Julien Pipard <j.pipard@useradgents.com>
2021-07-26 14:23:11 +0200

    hideKeyboard is no longer handled due to AppExtension lib support

--- Sources/MoreSwiftUI/SwiftUISugar.swift
+++ Sources/MoreSwiftUI/SwiftUISugar.swift
@@ -87,11 +87,3 @@ public extension View {
         }
     }
 }
-
-#if canImport(UIKit)
-public extension View {
-    func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
-#endif
