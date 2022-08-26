3352600a7f04ccc828a4e7d5532ca5327f066d09
Cyrille Legrand <c.legrand@useradgents.com>
2022-08-26 12:20:18 +0200

    More Bindings

--- Sources/MoreSwiftUI/Extensions/Binding.swift
+++ Sources/MoreSwiftUI/Extensions/Binding.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 public extension Binding {
-    static func get<T>(_ getter: @escaping () -> T) -> Binding<T> {
+    @inlinable static func get<T>(_ getter: @escaping () -> T) -> Binding<T> {
         Binding<T>(get: getter, set: { _ in })
     }
     
@@ -9,24 +9,72 @@ public extension Binding {
     /// (quotation marks because once the initial unwrap has passed, all subsequent unwraps are assumed to succeed.)
     /// Use this in "child views", where the parent view decides to show the child based
     /// on the ".some"-ness of an optional value.
-    func unwrap<Wrapped>() -> Binding<Wrapped>? where Optional<Wrapped> == Value {
+    @inlinable func unwrap<Wrapped>() -> Binding<Wrapped>? where Optional<Wrapped> == Value {
         guard wrappedValue != nil else { return nil }
         return Binding<Wrapped>(get: { wrappedValue! }, set: { self.wrappedValue = $0 })
     }
     
     /// Force-promote a `Binding<Value?>` to a `Binding<Value>`
-    func forceUnwrap<Wrapped>() -> Binding<Wrapped> where Optional<Wrapped> == Value {
+    @inlinable func forceUnwrap<Wrapped>() -> Binding<Wrapped> where Optional<Wrapped> == Value {
         return Binding<Wrapped>(get: { wrappedValue! }, set: { self.wrappedValue = $0 })
     }
     
+    @inlinable func nilIfEmpty() -> Binding<String> where String? == Value {
+        Binding<String>(
+            get: { wrappedValue ?? "" },
+            set: { wrappedValue = $0.nilIfEmpty }
+        )
+    }
+    
+
+
+    /**
+     If you have @State var selection: Int = 0
+     and you want a Binding<Bool> for every item of a list
+     use $selection.equals(4) for item 4
+     */
+    @inlinable func equals(_ value: Value) -> Binding<Bool> where Value: Hashable {
+        Binding<Bool>.init(
+            get: { wrappedValue == value },
+            set: { if $0 { wrappedValue = value }}
+        )
+    }
+
+
+    /**
+     If you have @State var selection: [Whatever] = [whatever1, whatever4]
+     (works with arrays or sets)
+     and you want a Binding<Bool> for every item of a list
+     user $selection.contains(whatever2) for item 2
+     */
+    @inlinable func contains<T: Hashable>(_ value: T) -> Binding<Bool> where Value == [T] {
+        Binding<Bool>.init(
+            get: { wrappedValue.contains(value) },
+            set: {
+                if $0 { if !wrappedValue.contains(value) { wrappedValue.append(value) }}
+                else { wrappedValue.removeAll(where: { $0 == value }) }
+            }
+        )
+    }
+
+    @inlinable func contains<T: Hashable>(_ value: T) -> Binding<Bool> where Value == Set<T> {
+        Binding<Bool>.init(
+            get: { wrappedValue.contains(value) },
+            set: {
+                if $0 { wrappedValue.insert(value) }
+                else { wrappedValue.remove(value) }
+            }
+        )
+    }
+
     /// Create a Binding suitable for using in previews
-    static func mock(_ value: Value) -> Self {
+    @inlinable static func mock(_ value: Value) -> Self {
         var value = value
         return Binding(get: { value }, set: { value = $0 })
     }
     
     /// Wrapper to listen to didSet of Binding
-    func didSet(_ didSet: @escaping ((newValue: Value, oldValue: Value)) -> Void) -> Binding<Value> {
+    @inlinable func didSet(_ didSet: @escaping ((newValue: Value, oldValue: Value)) -> Void) -> Binding<Value> {
         return .init(get: { self.wrappedValue }, set: { newValue in
             let oldValue = self.wrappedValue
             self.wrappedValue = newValue
@@ -35,7 +83,7 @@ public extension Binding {
     }
     
     /// Wrapper to listen to willSet of Binding
-    func willSet(_ willSet: @escaping ((newValue: Value, oldValue: Value)) -> Void) -> Binding<Value> {
+    @inlinable func willSet(_ willSet: @escaping ((newValue: Value, oldValue: Value)) -> Void) -> Binding<Value> {
         return .init(get: { self.wrappedValue }, set: { newValue in
             willSet((newValue, self.wrappedValue))
             self.wrappedValue = newValue
@@ -44,7 +92,9 @@ public extension Binding {
     
 }
 
-public func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+
+
+@inlinable public func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
     Binding(
         get: { lhs.wrappedValue ?? rhs },
         set: { lhs.wrappedValue = $0 }
